80ea9aba39a2d9ef22867e77e64fe6e100cadc8e
Peter Szilvasi <peti.szilvasi95@gmail.com>
2024-10-16 18:53:34 +0200

    Remove superflous HTML element

--- server.ts
+++ server.ts
@@ -6,17 +6,16 @@ import { serveStatic } from "hono/deno";
 
 const app = new Hono();
 
-// Simple in-memory storage for messages
+// Store and answer based on the most recent message
 let lastMessage = "";
 
-app.post("/send-message", async (ctx) => {
-  lastMessage = await ctx.req.json().then((res) => res.message);
-
-  return ctx.json({ success: true });
+app.post("/send-message", async (context) => {
+  lastMessage = await context.req.json().then((res) => res.message);
+  return context.json({ success: true });
 });
 
-app.get("/get-response", async (ctx) => {
-  if (!lastMessage) return ctx.json({ message: "" });
+app.get("/get-response", async (context) => {
+  if (!lastMessage) return context.json({ message: "" });
 
   const openai = new AzureOpenAI({
     endpoint: Deno.env.get("AZURE_ENDPOINT"),
@@ -29,17 +28,15 @@ app.get("/get-response", async (ctx) => {
     messages: [{ role: "user", content: lastMessage }],
   });
 
+  lastMessage = "";
+
   const message = result.choices[0].message.content;
   const markdownMessage = message != null ? message : "I can't answer.";
   const renderedMessage = marked.parse(markdownMessage);
-
-  lastMessage = "";
-
-  return ctx.json({ message: renderedMessage });
+  return context.json({ message: renderedMessage });
 });
 
-// Serve static files (JavaScript, HTML, CSS)
 app.use("/public/*", serveStatic({ root: "./" }));
-app.get("/", (c) => c.redirect("/public/index.html"));
+app.get("/", (context) => context.redirect("/public/index.html"));
 
 Deno.serve({ port: 8080 }, app.fetch);
